9bbbf97279b4dc090d8666d6324e2eff78b7d8a2
davidamarcosp <davidamarcosp@gmail.com>
2020-02-27 13:01:05 -0300

    Updates

---
+++ precache-manifest.bb7e565c3859fd52c26edd81552493ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9943a34eac118e6ef5043a94befe2ed9",
+    "url": "/spot-x-match/index.html"
+  },
+  {
+    "revision": "95793693f73117728196",
+    "url": "/spot-x-match/static/css/main.61d28bc1.chunk.css"
+  },
+  {
+    "revision": "dce4baeccafc78157a6e",
+    "url": "/spot-x-match/static/js/2.e9ade379.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/spot-x-match/static/js/2.e9ade379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95793693f73117728196",
+    "url": "/spot-x-match/static/js/main.0ae01127.chunk.js"
+  },
+  {
+    "revision": "040f0ae6490879f1d47b",
+    "url": "/spot-x-match/static/js/runtime-main.fac51ee3.js"
+  }
+]);
